bc8b5ecb4979be192208ad6389cfbda975c5cf78
AD4GO <116037611+AD4GO@users.noreply.github.com>
2023-03-20 18:55:08 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hola mundo- Admismistrador de Eventos</h1>
+<div>
+  prueba para ver si hacemos el proyecto de desarrollo web aqui
+  
+</div>
+`;
